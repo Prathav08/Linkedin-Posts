8df5edbe0b3cb44fbe7a9c7ea9eb536130312cb3
Prathav08 <75676782+Prathav08@users.noreply.github.com>
2025-08-28 16:03:05 +1000

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,17 @@
-import os
+# -------------------------------
+# CONFIG
+# -------------------------------
 
-# API Tokens
-RAINDROP_TOKEN = os.environ.get("RAINDROP_TOKEN")
-PERPLEXITY_API_KEY = os.environ.get("PERPLEXITY_API_KEY")
-LINKEDIN_ACCESS_TOKEN = os.environ.get("LINKEDIN_ACCESS_TOKEN")
+# Raindrop API
+RAINDROP_TOKEN = "b7d8b8b8-b588-4125-9ec0-1ac8c9d25a95"
 
-# Scheduler
-POST_DAYS = ["Tuesday", "Friday"]  # Days to post
-POST_HOUR = 9  # 9 AM Sydney time (best for engagement)
-POST_MINUTE = 30
+# Perplexity AI
+PERPLEXITY_API_KEY = "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"
+PERPLEXITY_MODEL = "sonar-pro"
 
-# Raindrop
-RAINDROP_LIMIT = 5
+# LinkedIn Page
+LINKEDIN_ACCESS_TOKEN = "AQWsKnxZqGi9vRByElrF4Xurl-pQfLtII2snL0mBVQ_pGWzyxs-b_N3YU15n5FKoBLTZZTFTeS3sv5hWwAuGb-6bhhCfGRyH3wTKDyFAO5o77g0DO_U8Cx_KyLvWD7utEAxqOgJYisILojzOzei_merDfFyD5D1R9bUwFrx-s06HQpd4mWJC_x7C5W5VTsMKnZc0CpxS99QozuoYqCYbcLdGEYeArQu0l2eHQwzL6HptHSc00HuYXaAeVWEMJLzJTvgsYV66HBbkch8uqNkFRJrCU_VYJqyxsnBoV6TiPMJ82dY3957XUzWRRozSh0Lnebu18tyuKd4y3LDUauLimuYlawssxQ"
+LINKEDIN_PAGE_URN = "urn:li:page:bpr;78c750be-65a6-470f-b005-964422653b5c"
+
+# Last run tracker
+LAST_RUN_FILE = "last_run.txt"
